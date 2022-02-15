5b823ccc1421230d98409c7c62179ee1ebc86f01
Iosif Turcu <iosif.turcu@emmanuel.ro>
2022-02-15 21:35:34 +0200

    initial setup

---
+++ eclipse_test/src/eclipse_test/Main.java
@@ -0,0 +1,13 @@
+package eclipse_test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//some logic will be inmplemented in here
+		int i = 0; 
+		System.out.println();
+	}
+
+}
