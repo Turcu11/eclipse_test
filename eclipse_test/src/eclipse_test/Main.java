6b43f977b29f208bc60a4c277fa3bf112f20cdf3
Iosif Turcu <iosif.turcu@emmanuel.ro>
2022-02-16 00:38:40 +0200

    2D ArrayList

--- eclipse_test/src/eclipse_test/Main.java
+++ eclipse_test/src/eclipse_test/Main.java
@@ -1,21 +1,41 @@
 package eclipse_test;
+import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) 
 	{
-		//some logic will be implemented in here
-		int i = 5;
-		System.out.println("Hi there");
-		System.out.println(factorial(i));
+		//System.out.println(factorial(5));
+		
+		//ArrayList2D();
 	}
 
-	
 	public static int factorial(int n)
 	{
 		if(n == 1)
 			return 1;
 		
 		return n * factorial(n-1);
+	}
+	
+	public static void ArrayList2D()
+	{
+		ArrayList<ArrayList<String>> listaMare = new ArrayList();
+		
+		ArrayList<String> listaBautura = new ArrayList();
+		listaBautura.add("suc");
+		listaBautura.add("apa minerala");
+		listaBautura.add("cafea");
+		
+		ArrayList<String> listaBrutarie = new ArrayList();
+		listaBrutarie.add("paine");
+		listaBrutarie.add("cornuri");
+		listaBrutarie.add("covrigi");
+
+		listaMare.add(listaBautura);
+		listaMare.add(listaBrutarie);
+		
+		System.out.println(listaMare);
 	}
 }
