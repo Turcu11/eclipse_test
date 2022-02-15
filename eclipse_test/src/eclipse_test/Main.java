954bd3c45b3d2ea3988918ed4a9bb1cd76860e61
Iosif Turcu <iosif.turcu@emmanuel.ro>
2022-02-16 00:10:28 +0200

    returns factorial using recursiv method

--- eclipse_test/src/eclipse_test/Main.java
+++ eclipse_test/src/eclipse_test/Main.java
@@ -2,13 +2,20 @@ package eclipse_test;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+	public static void main(String[] args) 
+	{
 		//some logic will be implemented in here
-		int i = 0; 
-		System.out.println(i);
+		int i = 6;
 		System.out.println("Hi there");
+		System.out.println(factorial(i));
 	}
 
+	
+	public static int factorial(int n)
+	{
+		if(n == 1)
+			return 1;
+		
+		return n * factorial(n-1);
+	}
 }
