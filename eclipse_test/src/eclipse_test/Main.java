4003ce97251f6db69b3be7d018b22071d8e63ef8
Iosif Turcu <iosif.turcu@emmanuel.ro>
2022-02-15 21:53:19 +0200

    setup

--- eclipse_test/src/eclipse_test/Main.java
+++ eclipse_test/src/eclipse_test/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//some logic will be inmplemented in here
+		//some logic will be implemented in here
 		int i = 0; 
 		System.out.println(i);
 		System.out.println("Hi there");
